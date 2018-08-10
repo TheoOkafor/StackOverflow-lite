98fe59cd59625b627c2ca7296b163405666bfe07
TheoOkafor <theo.okafor@yahoo.com>
2018-08-10 18:16:32 +0100

    style changed to airbnb style using eslint

--- UI/javascript/build/custom.bundle.js
+++ UI/javascript/build/custom.bundle.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
-//PASSWORD STRENGTH
-var result = document.getElementById("result");
-var password = document.getElementById("password");
-var passDisplay = document.getElementById("pass-strength");
+// PASSWORD STRENGTH
+var result = document.getElementById('result');
+var password = document.getElementById('password');
+var passDisplay = document.getElementById('pass-strength');
 
 var check = function check(input) {
   var result = void 0;
@@ -22,85 +22,85 @@ var check = function check(input) {
   };
 
   if (input.length < 6) {
-    result = "Minimum password length is 6";
+    result = 'Minimum password length is 6';
   } else if (input.length > 12) {
-    result = "Maximum password length is 12";
+    result = 'Maximum password length is 12';
   } else {
     var matchLength = doMatch(arr1, input).length;
     switch (matchLength) {
       case 4:
-        result = "Password Strength: 100%";
+        result = 'Password Strength: 100%';
         break;
       case 3:
-        result = "Password Strength: 75%";
+        result = 'Password Strength: 75%';
         break;
       case 2:
-        result = "Password Strength: 50%";
+        result = 'Password Strength: 50%';
         break;
       case 1:
-        result = "Password Strength: 25%";
+        result = 'Password Strength: 25%';
         break;
       default:
-        result = "Invalid";
+        result = 'Invalid';
     }
   }
   return result;
 };
 var checkPassword = function checkPassword() {
-  passDisplay.style.display = "inline-block";
+  passDisplay.style.display = 'inline-block';
   result.innerHTML = check(password.value);
 };
 
-//PASSWORD MATCH CHECK
-var matchDisplay = document.getElementById("match-display");
-var password2 = document.getElementById("password2");
-var signupBtn = document.getElementById("sign-up");
+// PASSWORD MATCH CHECK
+var matchDisplay = document.getElementById('match-display');
+var password2 = document.getElementById('password2');
+var signupBtn = document.getElementById('sign-up');
 
 var passwordMatch = function passwordMatch() {
   if (password.value !== password2.value) {
-    matchDisplay.innerHTML = "Passwords do not match!";
+    matchDisplay.innerHTML = 'Passwords do not match!';
   } else {
     signupBtn.disabled = false;
   }
 };
 
-//ACCEPT-ANSWER TOGGLE
+// ACCEPT-ANSWER TOGGLE
 
 var showAccepted = function showAccepted(input) {
   var index = parseInt(input);
-  var accepted = document.getElementsByClassName("accepted");
-  var unacceptBtn = document.getElementsByClassName("unaccept");
-  var acceptBtns = document.getElementsByClassName("accept");
+  var accepted = document.getElementsByClassName('accepted');
+  var unacceptBtn = document.getElementsByClassName('unaccept');
+  var acceptBtns = document.getElementsByClassName('accept');
 
-  unacceptBtn[index].style.display = "inline";
-  accepted[index].style.display = "inline";
+  unacceptBtn[index].style.display = 'inline';
+  accepted[index].style.display = 'inline';
 
   for (var i = 0; i <= acceptBtns.length - 1; i++) {
-    acceptBtns[i].style.display = "none";
+    acceptBtns[i].style.display = 'none';
   }
 };
 
 var showAcceptBtn = function showAcceptBtn(input) {
   var index = parseInt(input);
-  var accepted = document.getElementsByClassName("accepted");
-  var unacceptBtn = document.getElementsByClassName("unaccept");
-  var acceptBtns = document.getElementsByClassName("accept");
+  var accepted = document.getElementsByClassName('accepted');
+  var unacceptBtn = document.getElementsByClassName('unaccept');
+  var acceptBtns = document.getElementsByClassName('accept');
 
-  unacceptBtn[index].style.display = "none";
-  accepted[index].style.display = "none";
+  unacceptBtn[index].style.display = 'none';
+  accepted[index].style.display = 'none';
 
   for (var i = 0; i <= acceptBtns.length - 1; i++) {
-    acceptBtns[i].style.display = "inline";
+    acceptBtns[i].style.display = 'inline';
   }
 };
 
-//SHOW MORE CONTENT BUTTON
+// SHOW MORE CONTENT BUTTON
 var showMore = function showMore(input) {
   var index = parseInt(input);
-  var btnContainer = document.getElementsByClassName("btn-container");
-  //let showMoreBtn = document.getElementsByClassName("show-more");
-  var hiddenContent = document.getElementsByClassName("hidden-content");
+  var btnContainer = document.getElementsByClassName('btn-container');
+  // let showMoreBtn = document.getElementsByClassName("show-more");
+  var hiddenContent = document.getElementsByClassName('hidden-content');
 
-  hiddenContent[index].style.display = "inline";
-  btnContainer[index].style.display = "none";
+  hiddenContent[index].style.display = 'inline';
+  btnContainer[index].style.display = 'none';
 };
--- UI/javascript/custom.js
+++ UI/javascript/custom.js
@@ -1,107 +1,105 @@
-//PASSWORD STRENGTH
-let result = document.getElementById("result");
-let password = document.getElementById("password");
-let passDisplay = document.getElementById("pass-strength");
+// PASSWORD STRENGTH
+const result = document.getElementById('result');
+const password = document.getElementById('password');
+const passDisplay = document.getElementById('pass-strength');
 
 const check = (input) => {
   let result;
   const arr1 = [/[A-Z]+?/, /[a-z]+?/, /[0-9]+?/, /[$@#&!]+?/];
 
   const doMatch = (arr, word) => {
-    let result = [];
+    const result = [];
     let i;
 
-    for (i in arr){
-      if(word.match(arr[i]) !== null){
+    for (i in arr) {
+      if (word.match(arr[i]) !== null) {
         result.push(word.match(arr[i]));
       }
     }
     return result;
   };
-  
-  if(input.length<6){
-    result = "Minimum password length is 6";
-  }
-  else if(input.length>12){
-    result = "Maximum password length is 12";
-  }
-  else {
-    let matchLength = doMatch(arr1, input).length;
-    switch(matchLength){
-        case 4:
-        result = "Password Strength: 100%";
+
+  if (input.length < 6) {
+    result = 'Minimum password length is 6';
+  } else if (input.length > 12) {
+    result = 'Maximum password length is 12';
+  } else {
+    const matchLength = doMatch(arr1, input).length;
+    switch (matchLength) {
+      case 4:
+        result = 'Password Strength: 100%';
         break;
-    case 3:
-        result = "Password Strength: 75%";
+      case 3:
+        result = 'Password Strength: 75%';
         break;
-    case 2:
-        result = "Password Strength: 50%";
+      case 2:
+        result = 'Password Strength: 50%';
         break;
-    case 1:
-        result = "Password Strength: 25%";
+      case 1:
+        result = 'Password Strength: 25%';
         break;
-    default:
-        result = "Invalid";
+      default:
+        result = 'Invalid';
     }
   }
   return result;
-}
+};
 const checkPassword = () => {
-  passDisplay.style.display = "inline-block";
+  passDisplay.style.display = 'inline-block';
   result.innerHTML = check(password.value);
-}
+};
 
-//PASSWORD MATCH CHECK
-let matchDisplay = document.getElementById("match-display");
-let password2 = document.getElementById("password2");
-let signupBtn = document.getElementById("sign-up");
+// PASSWORD MATCH CHECK
+const matchDisplay = document.getElementById('match-display');
+const password2 = document.getElementById('password2');
+const signupBtn = document.getElementById('sign-up');
 
 const passwordMatch = () => {
-  if (password.value !== password2.value){
-    matchDisplay.innerHTML = "Passwords do not match!";
-  }else {
+  if (password.value !== password2.value) {
+    matchDisplay.innerHTML = 'Passwords do not match!';
+  } else {
     signupBtn.disabled = false;
   }
-}
+};
 
-//ACCEPT-ANSWER TOGGLE
+// ACCEPT-ANSWER TOGGLE
 
 const showAccepted = (input) => {
-  let index = parseInt(input);
-  let accepted = document.getElementsByClassName("accepted");
-  let unacceptBtn = document.getElementsByClassName("unaccept");
-  let acceptBtns = document.getElementsByClassName("accept");
+  const index = parseInt(input);
+  const accepted = document.getElementsByClassName('accepted');
+  const unacceptBtn = document.getElementsByClassName('unaccept');
+  const acceptBtns = document.getElementsByClassName('accept');
 
-  unacceptBtn[index].style.display = "inline";
-  accepted[index].style.display = "inline";
+  unacceptBtn[index].style.display = 'inline';
+  accepted[index].style.display = 'inline';
 
-  for (let i=0; i<=acceptBtns.length-1; i++){
-    acceptBtns[i].style.display = "none";
+  for (let i = 0; i <= acceptBtns.length - 1; i++) {
+    acceptBtns[i].style.display = 'none';
   }
-}
+};
 
 
 const showAcceptBtn = (input) => {
-  let index = parseInt(input);
-  let accepted = document.getElementsByClassName("accepted");
-  let unacceptBtn = document.getElementsByClassName("unaccept");
-  let acceptBtns = document.getElementsByClassName("accept");
+  const index = parseInt(input);
+  const accepted = document.getElementsByClassName('accepted');
+  const unacceptBtn = document.getElementsByClassName('unaccept');
+  const acceptBtns = document.getElementsByClassName('accept');
 
-  unacceptBtn[index].style.display = "none";
-  accepted[index].style.display = "none";
+  unacceptBtn[index].style.display = 'none';
+  accepted[index].style.display = 'none';
 
-  for (let i=0; i<= acceptBtns.length-1; i++){
-    acceptBtns[i].style.display = "inline";
+  for (let i = 0; i <= acceptBtns.length - 1; i++) {
+    acceptBtns[i].style.display = 'inline';
   }
-}
+};
 
-//SHOW MORE CONTENT BUTTON
+// SHOW MORE CONTENT BUTTON
 const showMore = (input) => {
-  let index = parseInt(input);
-  let btnContainer = document.getElementsByClassName("btn-container");
-  //let showMoreBtn = document.getElementsByClassName("show-more");
-  let hiddenContent = document.getElementsByClassName("hidden-content");
+  const index = parseInt(input);
+  const btnContainer = document.getElementsByClassName('btn-container');
+  // let showMoreBtn = document.getElementsByClassName("show-more");
+  const hiddenContent = document.getElementsByClassName('hidden-content');
 
-  hiddenContent[index].style.display = "inline";
-  btnContainer[index].style.display = "none";
-}
+  hiddenContent[index].style.display = 'inline';
+  btnContainer[index].style.display = 'none';
+};
