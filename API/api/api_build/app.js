c96bb75833b920d0e87d37a1754a40ea5305cb98
TheoOkafor <theo.okafor@yahoo.com>
2018-08-12 07:05:52 +0100

    Setup of Node/Express and required dependencies for development

---
+++ API/api/api_build/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+
+//parse application/json and look for raw text                               
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json' }));
+
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// listen for requests
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000");
+});
+
+module.exports = app;
