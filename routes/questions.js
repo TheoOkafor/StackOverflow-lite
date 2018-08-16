627fd4359009ea54a741076b0768670f291697b1
TheoOkafor <theo.okafor@yahoo.com>
2018-08-16 11:01:26 +0100

    Created the POST /answers API

--- routes/questions.js
+++ routes/questions.js
@@ -64,4 +64,40 @@ router.post('/v1/questions', (req, res) => {
   }
 });
 
+
+// POST AN ANSWER
+router.post('/v1/questions/:id([0-9]{1,})/answers', (req, res) => {
+  const reqBody = req.body; // reqBody = JSON.parse(req.body) worked for POSTMAN but not MOCHA test
+  const requestId = req.params.id;
+  const invalidReq = reqBody.body === null || reqBody.body === '' || reqBody.body === undefined;
+
+  // Find the question with the request id.
+  const currQuestion = questions.filter((question) => {
+    if (question.id === parseInt(requestId)) {
+      return true;
+    }
+  });
+
+  if (currQuestion.length < 1) {
+    res.status(404);// Set status to 404 as question was not found
+    res.json({ message: `Question ${requestId} Not Found` });
+  } else if (invalidReq) {
+    res.status(400);
+    res.json({ message: 'Bad Request. Answer must have a body.' });
+  } else {
+  	const answers = currQuestion[0].answers;
+    const newId = answers[answers.length - 1].id + 1;
+    const timeNow = new Date();
+    const timeStr = timeNow.toUTCString();
+    answers.push({
+      id: newId,
+      body: reqBody.body,
+      username: reqBody.username,
+      timeAnswered: timeStr,
+      accepted: false,
+    });
+    res.json({ message: `New answer: ${reqBody.body} added`, question: `${currQuestion[0].title}` });
+  }
+});
+
 module.exports = router;
