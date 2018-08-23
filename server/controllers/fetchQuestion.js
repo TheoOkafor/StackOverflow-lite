6c8a52aa1358c75038a017eaf9c8874cb40f2125
TheoOkafor <theo.okafor@yahoo.com>
2018-08-23 11:20:51 +0100

    [feature #159950825] Update get question with pg-promise and bluebird modules

--- server/controllers/fetchQuestion.js
+++ server/controllers/fetchQuestion.js
@@ -1,13 +1,32 @@
 import express from 'express';
 import client from '../db-setup';
+import db from '../db';
 
 const fetchQuestionCtrl = (req, res) => {
-  const requestId = req.params.id;
+  const requestId = parseInt(req.params.id);
   const query = {
     name: 'fetch-question',
     text: 'SELECT * FROM questions WHERE id = $1',
     values: [requestId]
   }
+
+  db.one(query.text, requestId)
+    .then( (data) => {
+      if(data.length === 0){
+        res.status(404);// Set status to 404 as question was not found
+        res.json({
+          status: 'failed',
+          message: `Question ${requestId} Not Found`,
+        });
+      } else {
+        res.json({
+          status: 'successful',
+          message: `Question ${requestId} found`,
+          data: data,
+        });
+      }
+    });
+/*
   client.query(query, (err, data) => {
     if(err || data.rows.length === 0){
       res.status(404);// Set status to 404 as question was not found
@@ -22,7 +41,7 @@ const fetchQuestionCtrl = (req, res) => {
         data: data.rows,
       });
     }
-  });  
+  }); */ 
 }
 
 export { fetchQuestionCtrl };
---
+++ server/db.js
@@ -0,0 +1,12 @@
+import promise from 'bluebird';
+
+const options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+const pgp = require('pg-promise')(options);
+const connectionString = 'postgresql://postgres:postgres@localhost:5432/stackLite';
+const db = pgp(connectionString);
+
+export default db;
