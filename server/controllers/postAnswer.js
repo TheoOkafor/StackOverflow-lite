ac5f8b26cb100eba4fe2edb646ec20fca0fc7c8f
TheoOkafor <theo.okafor@yahoo.com>
2018-08-23 13:08:23 +0100

    [feature #159950827] Integrate postgresQL DB to POST answer

--- server/controllers/postAnswer.js
+++ server/controllers/postAnswer.js
@@ -1,49 +1,46 @@
 import express from 'express';
-import { data } from '../../data/data';
-
-const questions = data.questions;
+import db from '../db';
 
 const postAnswer = (req, res) => {
   const reqBody = req.body;
   const requestId = req.params.id;
+  const timeNow = new Date().toUTCString();
   // Find the question with the request id.
-  const currQuestion = questions.filter((question) => {
-    if (question.id === parseInt(requestId)) {
-      return true;
-    }
-  });
-  if (currQuestion.length !== 1) {
-    const err = new Error(`Question ${requestId} Not Found`);
-    res.status(404);
-    res.json({
-      status: 'failed',
-      message: err.message,
-      data: reqBody,
-    });
-  } else {
-    const answers = currQuestion[0].answers;
-    const newId = answers[answers.length - 1].id + 1;
-    const timeNow = new Date().toUTCString();
-
-    const answer = {
-      id: newId,
-      body: reqBody.body,
-      username: reqBody.username,
-      timeAnswered: timeNow,
-      accepted: false,
-    };
-
-    answers.push(answer);
-    res.status(201);
-    res.json({
-      status: 'successful',
-      message: 'New answer added.',
-      data: answers[newId - 1],
-      metadata: {
-        location: `/v1/questions/${newId}`,
-      },
-    });
+  const request = {
+    text: 'WITH rows AS ('+
+      ' INSERT INTO answers'+
+      ' (questionID, body, timeSubmitted, username, accepted)'+
+      ' VALUES ($1, $2, $3, $4, $5)'+
+      ' RETURNING id'+
+      ')'+
+        'UPDATE questions SET answers = array_cat'+
+        '(topics, ARRAY[SELECT id FROM rows]) WHERE ID = $1'+
+          ' RETURNING id',
+    values: [requestId, reqBody.body, timeNow, reqBody.username, false],
   }
-};
+  db.one(request.text, request.values)
+    .then( (data) => {
+      if(!data){
+        const err = new Error(`Question ${requestId} Not Found`);
+        res.status(404);
+        res.json({
+          status: 'failed',
+          message: err.message,
+          data: reqBody,
+        });
+      } else {
+
+        res.status(201);
+        res.json({
+          status: 'successful',
+          message: 'New answer added.',
+          data: reqBody,
+          metadata: {
+            location: `/v1/questions/${requestId}/answers`,
+          },
+        });
+      }
+    });
+}
 
 export { postAnswer };
