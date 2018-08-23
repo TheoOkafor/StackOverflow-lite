a6ae87619b30b5ccb2e89739db9097ad000be98d
TheoOkafor <theo.okafor@yahoo.com>
2018-08-23 11:32:58 +0100

    [feature #159950826] Integrate postgresQL DB to POST question

--- server/controllers/postQuestion.js
+++ server/controllers/postQuestion.js
@@ -1,30 +1,34 @@
 import express from 'express';
-import { data } from '../../data/data';
-
-const questions = data.questions;
+import db from '../db';
 
 const postQuestion = (req, res) => {
   const reqBody = req.body;
-  const newId = questions[questions.length - 1].id + 1;
   const timeNow = new Date().toUTCString();
-  const newQuestion = {
-    id: newId,
-    title: reqBody.title,
-    body: reqBody.body,
-    timeSubmitted: timeNow,
-    username: reqBody.username,
-    answers: [],
-  };
-  questions.push(newQuestion);
-  res.status(201);
-  res.json({
-    status: 'successful',
-    message: 'New question added',
-    data: questions[newId - 1],
-    metadata: {
-      location: `/v1/questions/${newId}`,
-    },
-  });
+  const request = {
+    text: 'INSERT INTO questions (title, body, timeSubmitted, username,'+
+    ' answers) VALUES ($1, $2, $3, $4, $5) RETURNING id',
+    values: [reqBody.title, reqBody.body, timeNow, reqBody.username, []],
+  }
+  db.one(request.text, request.values)
+    .then( (data) => {
+      if(!data){
+        res.status(501);// Set status to 501
+        res.json({
+          status: 'failed',
+          message: 'Server failed to complete request',
+        });
+      } else {
+        res.status(201);
+        res.json({
+          status: 'successful',
+          message: 'New question added',
+          data: reqBody,
+          metadata: {
+            location: `/v1/questions/${data.id}`,
+          },
+        });
+      }
+    })
 };
 
 export { postQuestion };
