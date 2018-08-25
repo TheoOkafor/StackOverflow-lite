0762f3ba651c448c06472a0b47b993ca0e3a58e9
TheoOkafor <theo.okafor@yahoo.com>
2018-08-25 16:03:09 +0100

    [chore #159912987] Modify urlErrHandler middleware and tests

--- server/middlewares/urlErrHandler.js
+++ server/middlewares/urlErrHandler.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const urlErrHandler = (req, res) => {
-  const err = new Error('Bad Request, URL parameter must be integer');
+  const err = new Error('Bad Request, URL ID parameter must be integer');
   res.status(400);
   res.json({
     status: 'failed',
