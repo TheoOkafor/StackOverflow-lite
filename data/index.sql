74b036947852a521127165ffad0fadb9bb3a46cb
TheoOkafor <theo.okafor@yahoo.com>
2018-08-29 11:22:59 +0100

    [chore #160047696] Edit Database name and sql files

--- data/index.sql
+++ data/index.sql
@@ -1,9 +1,7 @@
-#DROP DATABASE IF EXISTS stackLite;
-#CREATE DATABASE stackLite;
-#\c stackLite;
-#DROP TABLE answers;
-#DROP TABLE questions;
-#DROP TABLE users;
+\c stackLite;
+DROP TABLE answers;
+DROP TABLE questions;
+DROP TABLE users;
 \ir ./users.sql
 \ir ./questions.sql
 \ir ./answers.sql
