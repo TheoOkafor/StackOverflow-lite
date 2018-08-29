98d14d154fe592bfb6b96dcd65af7d3c14b51408
TheoOkafor <theo.okafor@yahoo.com>
2018-08-29 06:01:53 +0100

    [chore #160047696] Update index.sql

--- data/index.sql
+++ data/index.sql
@@ -1,6 +1,9 @@
-DROP TABLE answers;
-DROP TABLE questions;
-DROP TABLE users;
+DROP DATABASE [ IF EXISTS ] stackLite;
+CREATE DATABASE stackLite;
+\c stackLite;
+#DROP TABLE answers;
+#DROP TABLE questions;
+#DROP TABLE users;
 \ir ./users.sql
 \ir ./questions.sql
 \ir ./answers.sql
