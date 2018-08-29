55cd89d542e1378c534b7d30754c653b1b9b4296
TheoOkafor <theo.okafor@yahoo.com>
2018-08-29 05:53:20 +0100

    [chore #160047696] Update index.sql

--- data/index.sql
+++ data/index.sql
@@ -1,9 +1,6 @@
-DROP DATABASE stackLite;
-CREATE DATABASE stackLite;
-
-#DROP TABLE answers;
-#DROP TABLE questions;
-#DROP TABLE users;
+DROP TABLE answers;
+DROP TABLE questions;
+DROP TABLE users;
 \ir ./users.sql
 \ir ./questions.sql
 \ir ./answers.sql
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,28 +2,36 @@ import express from 'express';
 import db from '../db';
 
 const user = (req, res, next) => {
-	User.findById(req.userId, { password: 0 }, (err, user) => {
-    if (err){
-    	res.status(500);
-    	res.json({
-    		status: 'failed',
-    		massage: 'Error occurred while finding user',
-    	});
-    }
+	const request = {
+    text: 'SELECT username, email, created, modified FROM users'
+    +' WHERE ID=$1',
+    values: [req.id],
+  };
+	db.one(request.text, request.values) 
+		.then((user) => {
     if (!user) { 
-    	res.status(404);
-    	res.json({
-    		status: 'failed',
-    		message: 'User not found',
-    	});
-    }
-    res.status(200);
-    res.json({
-    		status: 'successful',
-    		message: 'User found',
-    		data: user,
-    });
-  });
+	    	res.status(404);
+	    	res.json({
+	    		status: 'failed',
+	    		message: 'User not found',
+	    	});
+	    }
+	    res.status(200);
+	    res.json({
+	    		status: 'successful',
+	    		message: 'User found',
+	    		data: user,
+	    });
+	  })
+	  .catch( error => {
+	  	if (error){
+	    	res.status(500);
+	    	res.json({
+	    		status: 'failed',
+	    		massage: 'Error occurred while finding user',
+	    	});
+	    }
+	  });
 }
 
 export default user;
