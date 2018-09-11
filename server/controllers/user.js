034823d50c1f845dedadefd64f77adc2ff169f57
TheoOkafor <theo.okafor@yahoo.com>
2018-09-11 09:27:31 +0100

    [chore #160246959] Modify GET users route for fetch API

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -9,9 +9,25 @@ const user = (req, res, next) => {
     SELECT * FROM answers WHERE userid=$1`,
     values: [req.params.id],
   };
-	db.multi(request.text, request.values) 
-		.then((user) => {
-    	if (user[0].length<1) { 
+
+  db.task('get-user-everything', t => {
+  	return t.multi(`SELECT username, email, created, modified FROM users
+    WHERE ID=$1; SELECT * FROM answers	WHERE USERID=$1;`, req.params.id)
+      .then(result => {
+      	const user = [];
+      	user.push(result);
+		  	return t.map(`SELECT * FROM questions	WHERE USERID=$1;`, req.params.id, question => {
+		  		return t.any('SELECT * FROM answers WHERE questionid=$1', question.id)
+		  			.then(answers => {
+		  				question.answers = answers;
+		  				user.push(question);
+		  				return user;
+		  			})
+		  	});
+  		}).then(t.batch);
+  })
+  	.then(output => {
+  		if (output.length<1) { 
 	    	res.status(404);
 	    	res.json({
 	    		statusCode: 404,
@@ -19,31 +35,25 @@ const user = (req, res, next) => {
 	    	});
 	    	return res;
 	    } else {
-	    	const userDetails = user[0][0];
-	    	const data = {
-	    		userDetails,
-	    	};
-	    	data.userDetails.questions= user[1];
-	    	data.userDetails.answers= user[2];
-
-		    res.status(200);
+	    	let result = output[0];
+	    	let questions = [];
+	    	for (let i=1; i<result.length; i++) {
+	    		questions.push(result[i]);
+	    	}
+	    	let data = result[0][0][0];
+	    	data.answers = result[0][1];
+	    	data.questions = questions;
+				res.status(200);
 		    res.json({
 		    		statusCode: 200,
 		    		message: 'User found',
 		    		data,
 		    });
 		    return res;
-		  }
-	  })
+	  	}
+  	})
 	  .catch( error => {
-	  	if (error){
-	    	res.status(500);
-	    	res.json({
-	    		statusCode: 500,
-	    		error: 'Error occurred while finding user',
-	    	});
-	    	return res;
-	    }
+	  	console.log(error);
 	  });
 }
 
